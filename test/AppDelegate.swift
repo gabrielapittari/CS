dc6d3a2338187db5a6187f608afca79a21526c40
Gabriela Pittari <gabrielapittari@gmail.com>
2017-12-04 11:03:50 +0000

    First Commit

---
+++ test/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  test
+//
+//  Created by Gabriela Pittari on 04/12/2017.
+//  Copyright © 2017 cs. All rights reserved.
+//
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        /// start new window
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let moviesViewController = ViewController()
+        let navigationController = UINavigationController(rootViewController: moviesViewController)
+        window!.rootViewController = navigationController
+        window!.makeKeyAndVisible()
+        return true
+    }
+}
